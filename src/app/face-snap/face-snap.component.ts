b1394dc106bcba267a0ece9894c5a365cb7bff09
HJC <huchardjc@gmail.com>
2024-10-01 18:25:51 +0200

    update FaceSnapComponent and add style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createAt!: Date;
+  snaps!: number;
+  imageURL!: string;
+  snapButtonText!: string;
+  isSnap!: boolean;
 
+  ngOnInit(): void {
+    this.title = 'Vegeta';
+    this.description = 'Best anime character';
+    this.createAt = new Date;
+    this.snaps = 154564;
+    this.imageURL = 'https://static.wikia.nocookie.net/dragonball/images/2/2c/Vegeta_Manga.png';
+    this.snapButtonText = "Oh Snap!";
+    this.isSnap = false;
+  }
+  onSnap(): void {
+    if (this.isSnap === false){
+    this.snaps++;
+    this.isSnap = true;
+    this.snapButtonText = "Oops, un Snap";
+  } 
+    else if (this.isSnap === true){
+      this.snaps--;
+      this.isSnap = false;
+      this.snapButtonText = "Oh Snap!";}
+  }
 }
